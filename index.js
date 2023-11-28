e8ba30f04108ebe345a568ee8a8c1c9db445518a
renantm1 <renan.takinami@gmail.com>
2023-11-28 10:49:39 -0300

    popup

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function togglePopup() {
     const popup = document.getElementById('popup');
     const overlay = document.getElementById('overlay');
 
-    // Alterna a visibilidade do pop-up e da sobreposição
     popup.classList.toggle('show-popup');
     overlay.classList.toggle('show-popup');
 }
